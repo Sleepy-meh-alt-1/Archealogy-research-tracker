5f4c44ad0d40052f1d49b6d6c0d6e7122e4474a3
Sleepy-meh <135198078+Sleepy-meh-alt-1@users.noreply.github.com>
2026-01-29 16:41:11 +0100

    updated json

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,27 @@
 
 const RESEARCH_ICON = "HiMm/x4iJf8eIyX/HSAl/x0gJP8cICP/HB8j/xwgI/8cHyP/HB8j/xwfI/8cICP/HCAk/xwfI/8cHyP/HB8j/xwgJP8cICT/HSAk/x0gJP8dICT/HiIl/x8kJ/8fIyb/HiIm/x0jJf8gIyb/HiIm/x0iJf8dIiX/HSIk/x4iJf8eIyb/HiIl/x4jJf8cHyT/HB8j/xwfI/8cHyP/HB8j/xwfI/8cHyP/HB8j/xwgI/8cICT/HB8j/xwfI/8cHyP/HCAk/xwgJP8cICT/HSAk/x0iJf8eIib/HiIm/x0iJf8cICT/HCEk/x0iJf8dIiX/HSIl/x0iJf8dIiX/HyMn/x4jJv8eIiX/HiMl/x0gJf8dICT/HCAj/xwfI/8cICP/HB8j/xwfI/8cHyP/HCAj/xwgJP8cHyP/HB8j/xwfI/8cICT/HCAk/x0gJP8dICT/HSAk/x4iJf8fJCf/HyMm/x4iJv8dIyX/ICMm/x4iJv8dIiX/HSIl/x0iJP8eIiX/HiMm/x4iJf8eIyX/HB8k/xwfI/8cHyP/HB8j/xwfI/8cHyP/HB8j/xwfI/8cICP/HCAk/xwfI/8cHyP/HB8j/xwgJP8cICT/HCAk/x0gJP8dIiX/HiIm/x4iJv8dIiX/HCAk/xwhJP8dIiX/HSIl/x0iJf8dIiX/HSIl/x8jJ/8eIyb/HiIl/x4jJf8dICX/HSAk/xwgI/8cHyP/HCAj/xwfI/8cHyP/HB8j/xwgI/8cICT/HB8j/xwfI/8cHyP/HCAk/xwgJP8dICT/HSAk/x0gJP8eIyb/HSMm/x0iJf8eISX/HSAk/x0fI/8cICT/HCAl/xwhJP8cHyP/Gx8j/xsfI/8cHyP/GyAk/x0gJP8cICT/HSAk/x0gJP8dIST/HSIl/x0iJf8fIyb/ICQn/yAlKP8fIyf/HyMn/x8jJv8dIiX/HiIm/x0iJf8dIiT/HSIl/x0iJf8eIyb/HSIl/x4hJf8dICT/HR8j/xwgJP8cICX/HCEk/xwfI/8bHyP/Gx8j/xwfI/8bICT/HSAk/xwgJP8dICT/HCAk/xwfI/8cICT/HSIl/x4jJv8fIyb/HiMl/x4iJv8eIyb/HSMm/x4jJv8dIiX/HSIk/x0iJf8fIyb/HiMm/x0jJv8dIiX/HiEl/x0gJP8dHyP/HCAk/xwgJf8cIST/HB8j/xsfI/8bHyP/HB8j/xsgJP8dICT/HCAk/x0gJP8dICT/HSEk/x0iJf8dIiX/HyMm/yAkJ/8gJSj/HyMn/x8jJ/8fIyb/HSIl/x4iJv8dIiX/HSIk/x0iJf8dIiX/HiMm/x0iJf8eISX/HSAk/x0fI/8cICT/HCAl/xwhJP8cHyP/Gx8j/xsfI/8cHyP/GyAk/x0gJP8cICT/HSAk/xwgJP8cHyP/HCAk/x0iJf8eIyb/HyMm/x4jJf8eIib/HiMm/x0jJv8eIyb/HSIl/x0iJP8dIiX/HyMm/x4jJv8dIyb/HSIl/x4hJf8dICT/HR8j/xwgJP8cICX/HCEk/xwfI/8bHyP/Gx8j/xwfI/8bICT/HSAk/xwgJP8dICT/HSAk/x0hJP8dIiX/HSIl/x0iJf8dIiX/HiIm/x0iJf8dIiX/HSIl/x4jJv8eIyb/HiIl/x0hJf8cICP/HB8j/xwfI/8cHSL/HB8j/x0iJv8dICT/HSEk/x0iJf8dIiX/HSIl/x0iJf8eIiX/ICYp/x8jJv8eIyb/HiMm/x8kJ/8eIyb/HSIl/x4iJf8eIiX/HiIl/x4jJv8fIyf/HSIl/x0iJf8dIiX/HiMm/x4jJv8eIiX/HSEl/xwgI/8cHyP/HB8j/xwdIv8cHyP/HCEk/x0hJf8dIiX/HSEk/x0iJf8dIiX/HiIm/x8jJ/8fIyf/HyMm/x8jJ/8fIyb/HiMm/x8jJ/8eIib/HiIm/x4iJf8dIiX/HSIl/x4iJv8dIiX/HSIl/x0iJf8eIyb/HiMm/x4iJf8dISX/HCAj/xwfI/8cHyP/HB0i/xwfI/8dIib/HSAk/x0hJP8dIiX/HSIl/x0iJf8dIiX/HiIl/yAmKf8fIyb/HiMm/x4jJv8fJCf/HiMm/x0iJf8eIiX/HiIl/x4iJf8eIyb/HyMn/x0iJf8dIiX/HSIl/x4jJv8eIyb/HiIl/x0hJf8cICP/HB8j/xwfI/8cHSL/HB8j/xwhJP8dISX/HSIl/x0hJP8dIiX/HSIl/x4iJv8fIyf/HyMn/x8jJv8fIyf/HyMm/x4jJv8fIyf/HiIm/x4iJv8eIiX/HSIl/x0iJf8eIib/HSIl/x0iJf8dIiX/HiMm/x4jJv8eIiX/HSEl/xwgI/8cHyP/HB8j/xwdIv8cHyP/HSIm/x0gJP8dIST/HSIl/x0iJf8dIiX/HiMm/x8jJv8eIyb/HiIl/050jv94vO7/eb7w/22q1v9ztOP/VoGh/yQuNv8dIST/HCEk/xwhJP9Nco3/eLzu/3m+8P9tqtb/c7Ti/2CVu/8fJSn/HSIl/ztUZv9gkrj/YZW6/22o1P9EZHn/ISgq/050j/94vO7/eb7w/22q1v9ztOP/YZa7/yEnK/8fIyf/HiMl/x4iJf8eIyb/TnSP/zdNXf8fJCf/HSIl/x0hJP8cIST/TXOO/3i87f95vvD/barW/3O04/9WgaH/JS42/x4iJf8eIiX/HiIm/x8jJ/8lLTT/TnSP/2aex/9tqdX/banV/22p1f9hlrv/ICUp/050j/94vO3/c7Tj/z5ZbP8fJCf/HiIl/050j/94vO3/c7Tj/z5abP8eJCf/HSEk/xwhJP8cIST/HCAj/xweI/9Oc47/eLzt/3m+8P9tqtb/c7Ti/22p1f8fJSn/TnOO/3i87f9ztOP/P1pu/yEnKv8lLTT/XpG2/yYvN/8eIib/HyMm/x4jJv8lLjT/VH6d/3i87v9WgqH/ICYq/05zj/94vO3/eb7w/22q1v9ztOL/c7Ti/2agyP9JbIX/HiMo/x0iJv8dIyb/TXOP/3i87f9ztOL/Pllt/x8kKP8lLTT/X5K2/ycwN/8fIyb/HyMm/x8jJv8lLTP/U32d/3i87f9WgqL/ISYq/x4jJv8eIiX/R2mB/2adxf9tqdX/c7Tj/22p1f9ci67/HyUp/x0hJP8cIST/OlNm/1+Stv9gk7n/bajU/0Nief8eJCj/HSIl/x0iJf8fJCf/HiMm/x8jJv8eIyb/HyMm/3e66/9ajLL/AgME/zJNYf93u+3/QGJ7/x0jJf8eIiX/HiIm/x4iJf93uuv/Woyy/wIDBP8EBQX/OVhu/wkLDf81Slr/d7vs/yMxO/8JCgv/FyAo/zpacf8TFxn/HiEl/3e66/9ajLL/AgQF/wQFBf85WG//CQwN/x4jJv8fIyb/HyQn/yUtM/9xsN7/XZG2/xgdIP8eIyb/HiMm/x4iJf8fIyf/d7rr/1qMsv8CBAX/Mk1g/3e77P9BY3z/HiQm/x8jJ/8fIyb/JS0z/2um0f9glbv/Fx8l/wYICf8EBQX/FB0j/2CWvf8LDQ//HiMm/3a66v9ajLL/BAYI/xQWGP8fJCf/HyMm/3a66v9ajLL/BAYI/xMWGP8eIyb/HiIl/x4iJv8eIiX/HSEk/x0fJP92uer/Woyy/wMEBf8EBQX/OFhv/wYHCP8dIiX/drnq/1qMsv8FBgj/ExYY/yk1P/93uuv/Y5vD/yQuNf8fIyb/ICQo/x8kJ/8dIiT/drnq/xMeJv8MDhD/HiIl/3a56v9ajbL/AwQF/wQFBf8SGiH/R3CO/3i87f9lncX/JjA3/x4iJf8dIyX/drnq/1qNsv8FBgj/FBYY/yo3QP93uuv/Y5vD/yUuN/8fIyf/HyQn/x8jJv8bICP/drnp/xMeJ/8MDhD/HiIl/1+Rtf9nn8j/IS43/wcICf8EBQX/KT9Q/2eizf8NERP/HSIl/zVLWf93uuv/JDI8/wkKC/8XHyj/Ollx/xMWGv8eIiX/HiIl/x8kJ/8fJin/HiMm/x8jJv8fJCf/d7rr/1qMsv8KDA7/HiIl/3e66/9ajLL/EhYY/x4jJv8eIyb/HiIl/3e66/9ajLL/CgwO/x0hJf8eIiX/DxMU/0dpgf94vO3/IjdE/xkeIf8dISX/Hygu/w8TFP8cICT/d7rr/1qMsv8KDA7/HSIl/yAkKP8RFBX/HyMn/x8kJ/8fJCf/SGmC/3Ky4P95vvD/ITE9/x4jJv8eIyb/HiMm/x8jJ/93uuv/Woyy/woMDv8eIiX/d7rr/1qMsv8TFhn/HyMn/x8jJ/9OdI//eLzt/xciKv8ICgv/HCAj/x8jJv8cIiX/OVJk/wcJC/8eIyb/drrq/1qMsv8KDQ//HiMm/x8kJ/8fIyb/drrq/1qMsv8KDQ//HSIl/x4jJv8eIyb/HiMm/x4iJf8dIiX/HSIk/3a66v9ajLL/Cg0P/xwiJP8eIib/ERIV/x8jJv92uer/Woyy/woND/8bICP/KDQ+/3e66/95vvD/aqfS/yo5Qv8gJSn/ICUo/x8jJ/92uer/Ex4m/xseIf8eIyb/drnq/1qNsv8KDQ//HSIl/x4jJv8bHiL/S3WS/3i87f8+X3j/HiMl/x8kJ/92uer/Wo2y/wsND/8eIib/KzdA/3e66/95vvD/aqfS/ys6RP8fJCf/HCIl/x4jJv92uer/Ex4n/xshJP9BX3T/d7vs/yIzPv8HCAr/Gh0g/x4jJv8fIyb/SW6J/wYICf8dIiX/R2mB/3e77P8jOEb/GR4h/x0iJP8fKC7/EBMV/x4hJP8dIyX/HyQn/x8kJ/8fJCf/HyQn/x8kJ/93uuv/Woyy/woNDv8eIyb/d7rr/zpbc/8JDA3/HiIl/x4iJf8eIiX/d7rr/1qMsv8KDA7/HSEl/yUtM/8eIyb/MEBN/3e77P95vvD/THOQ/x8lKf8eIyb/HCAj/x4iJv93uuv/Woyy/woMDv8fIyb/Ji81/yEmKf8gJCj/IScp/x8kJ/9rptD/L0RT/3e77P9KdJP/Gx8i/x8kJ/8fIyb/HiMm/3e66/9ajLL/CgwO/x4jJv93uuv/Olx0/woMDv8fIyf/ICUn/2ul0P9ysuH/AwUG/xseIv8fJCf/HyMm/x8kJ/8gJCj/FRga/x8kJ/92uur/Woyy/woOD/8eIyb/HyQn/yAlKP92uur/Woyy/woOD/8fIyb/HyQn/x8jJv8eIiX/HiIl/x4jJv8fIyb/drrq/1qMsv8KDQ//HSIl/yQsM/8eIyb/HiIl/3a56v9ajLL/Cg0P/x0hJf8qNkD/ca/d/zJPZP93u+z/cbLg/zdNXv8iJyr/ICQo/3a56v8THib/HCAj/x8kJ/92uer/Wo2y/wsOD/8eIyb/HyMm/x4jJv8qNj//d7rr/2KZwf8TFxr/HyMm/3a56v9ajbL/DA4Q/x8kJv8sOEH/ca/d/zJPZP93uuz/cbLh/zZMXP8gJSn/ICQo/3a56v8THif/Gx8i/2Wbw/9ysuH/BAYH/xgcHv8gJSj/HyQn/x8kJ/8fJCf/EBIT/x4jJv8wQU3/d7rr/3m+8P9NdJD/ICYp/x4jJv8cICP/HyMm/x4jJv8gJSj/ICQo/x8kJ/8gJSj/ICQo/3e66/9ajLL/Cg0P/0FedP9gk7j/AgQF/xATFP8fIyb/HyMm/x8kJ/93uuv/eb7w/2qn0v9tqdX/VoGh/x4kJ/8fIyf/OFFj/3Cv3f95vvD/XZG2/yEnK/8fJCf/HyMn/3e66/95vvD/aqfS/22q1f9XgqH/ICUq/yEmKf8gJCj/Nkxc/2adxf8HCQr/XY6y/3i97v8WHiT/HiMm/x8jJv8fIyf/d7rr/1qMsv8KDA7/QF90/2CUuP8DBAX/ERIT/x8jJv8fJCf/d7rr/2mm0v8FBgn/HiMm/yAlKP8hJin/ISUo/yElKP8gJSj/ICUo/3e66v95vvD/aqfS/22p1f9tqdX/banV/3m97/9ajLL/Cg0P/x0iJf8eIyb/HyMm/x8jJv8fJCf/HyQn/x8kJ/92uur/eb7w/2qn0v9tqdX/UHeU/x4jJ/8fJCf/drnq/1qMsv8KDg//HiMm/ys3Qf93uuv/BggK/0Fhef93u+3/cbLg/zRKW/8iJyr/drnq/wsSF/8aHSH/HyUo/3a56v9ajbL/Cg0P/x0iJf8fIyb/HyMn/x8kJ/92uer/aabR/wkMDv8eIib/drnq/1qNsv8LDQ//HiMm/ys4Qf93uuv/BggK/0FheP93u+z/cbLh/zRJWf8iJin/drnq/wwTF/8bHiL/drnp/2qn0v8FBwr/HiMm/x8jJ/8fJSj/HiIm/x4iJf8eIyb/HiMm/x8jJv84UGL/b67c/3m+7/9dkrf/Iigs/x8kJ/8gJCf/HyQn/yAlKP8gJSj/ICUo/x8kJ/8fJCf/d7rr/3m+8P9qp9L/eb3v/z1bcf8JCwz/HSIl/x4jJv8gJCj/HyQn/3e66/9ajLL/AgME/wwRFP8yTWH/DhES/yAjJ/8gJCj/Ii43/1iKrv94ve7/MElb/x8kJ/8fJCf/d7rr/1qMsv8CBAX/DBEV/zJNYf8OERL/IScq/yEmKf9fkrb/crLh/2qm0f9ztOL/eb7w/zpcc/8dIyX/HiMm/x8kJ/93uuv/eb7w/2qn0v95ve//Plxx/wkLDf8eIyX/HyQn/x8kJ/9rpdD/eb3v/w8WG/8eIyf/ICUo/yAjKP8hJij/ISYo/yAlKP8gJSj/d7rq/1qMsv8DBAX/BAUF/wQFBf8EBQX/drnp/1qMsv8LDg//HSIl/x4jJv8eIyb/ICQo/x8kJ/8fJCf/ICQn/3a66v9ajLL/AwQF/wwRFP8yTWH/EBMV/yAjJ/92uer/Woyy/woOD/8eIiX/KzhB/3e66/8EBgf/HyQo/zNMXf93u+z/cbLg/zVLW/93uuv/BAYH/xsgIv8eIyb/drnq/1qNsv8KDQ//HSIl/x4jJv8fJCf/JS00/3a66v9ajbL/BwkK/x4iJv92uer/Wo2y/wsOD/8eIyb/KzhB/3e66/8EBgj/HiMm/zNMXP93uuv/cbLh/zRKWf93uuv/BAYI/x0iJP92uer/cbHh/wgLDP8eIiX/HiMm/ztVZ/9xsd7/eb7w/0Rke/8fJSn/HiMm/yAkKP8iLjf/V4mt/3i87v8wSlz/ICYq/yAlKP8gJSf/Iicr/yInK/8iJyr/ICUn/x8jJ/93uuv/Woyy/wIDBP9QfZ7/eLzu/zBFVf8gJSn/ICQo/yAkKP8gJSj/d7rr/1qMsv8LDQ//ICQn/x8jJv8UFxn/PVZp/yEmKv8jJin/Iiox/3e66/86XHP/Fhsd/yAlKP93uuv/Woyy/wsND/8gJCf/HSIk/xMWGP8iJin/LThD/3Gx3/8TGR7/AgMD/wQFBf9urNr/aaXQ/xMXGv8gJCf/HyQn/3e66/9ajLL/AgQF/1B9nv94vO7/MEZV/x8lKP8gJSj/HyQn/050j/94vO3/Olx0/x4kJv8gJSj/ICUo/yEmKP8uPEP/Iyku/yInK/93uuv/Woyy/wsOD/8eIib/HiIm/x4jJv92uur/Woyy/woND/8eIiX/ICQo/yAkKP8gJCj/ICUo/yAlKP8gJSj/drrq/1qMsv8LDhD/ICUo/x4jJf8TFhj/IyYp/3a66v9ajLL/Cg4P/x8kJ/83TVv/d7vs/wQGB/8gJSj/ISUo/zBDUv9vrtz/eb7w/3m+8P8EBgf/HSIl/x8iJf92uer/Wo2y/woND/8fIyf/ICQo/x8kJ/88VWf/d7vs/ypDVf8KDA3/HiMm/3a56v9ajbL/DA4Q/x4jJv82TFv/d7rr/wQGCP8eIyb/ICUo/y9EU/9vrtz/eb7v/3m+8P8EBgj/ISYq/2CStv94ve7/LUVX/x4jJ/8eIyb/HyIl/1yNsv94ve7/BAYI/xIVF/89VWj/ISYq/yAlKP8iKzH/drnq/zpcdP8XGx7/ISYp/yEmKf8jKi7/Iygs/yInKP8iKCr/ISYp/3e66/9ajLL/Cw0P/yYvNf9bjbH/cLDf/zNJWP8fJij/ICUo/yAlKP93uuv/YpnB/wsOEP8fJSf/RGF3/ykxOP9rp9H/ICct/yAlKf8xQk7/d7vs/xoqNf8TFRn/JSkt/3e66/9imcH/Cw4Q/x8iJ/9DYHb/JC01/yEmKf9PdZD/Womr/wQHB/8fIiX/ISYp/1WAnv94vO7/JTpJ/x8lKP8gJSj/d7rr/1qMsv8LDQ//JC0z/1uMsP9wsN//M0lZ/x8lKP8gJSj/JS40/2KYwP94ve//RGV+/yEmKv8gJCj/ICUo/1+St/8hJin/Iyou/3e66/9ajLL/Cw8Q/yAlKP8gJSj/IyYp/3e66v9ajLL/Cw4Q/yAlKP8gJCf/HyUn/yAlKP8gJSj/ICUo/yAlKP92uur/Woyy/wsOEP8hJSj/Iigp/yMmKf8gJSn/drrq/1qMsv8LDg//Iics/zpQYP93u+z/CxIX/yEmKP8gIyj/ICUo/yk5Rf9vrtv/eb7w/wQGB/8iJSj/IiYp/3a56v9imcH/DA4Q/x8jJv8fJSf/N01b/3Gx3/9ZiKv/AwQF/xQXGf8gJSj/drnq/1qNsv8LDg//HyQn/zZMW/93uuv/DBMX/x4jJv8gJCj/ICQo/ys7Rv9vrdv/eb7v/wQGCP8iJyv/LjpC/2ijzP9xseD/OFNm/yQnK/8iJin/YJG2/3i97v8EBgj/HyMm/2qmz/8fKC//ICUo/zFDT/93uuv/Gys2/xEVFv8hJij/IiYp/yAlJ/8iJyv/Iygt/yUqLf9Ka4T/eLzt/3Gx4P8+YHn/JCku/ycwN/9TgKH/eL3u/1N+nv8nMTj/SWqD/3i87f95vvD/cbLh/22q1v9ztOP/Fhse/1+Stv9xsd//banV/22p1f86VWf/AQID/xsfIf9Ka4T/eLzt/3m+8P9xsuH/barV/3O04/8WGx7/SmuD/3Ky4f9ekbb/FBwh/yInKv8jKCz/RGN5/3i87f9pptH/OVNm/0prhP94vO3/cbHg/z5gef8hJyr/JC0z/1OAof94vO7/U36e/ycxOP8fJCf/Iywy/ztbcv9glr7/crLg/22q1f9ztOP/XYyv/wsOEP9IaYL/eLzt/3Gx4P8/YXn/Iyks/yEkJ/9PdpH/eLzt/3Gx4P8+YXn/JCou/yInKf8fJCf/ICUo/yAlKP8hJin/SWuD/3i87f9xseD/P2B5/yMoK/8hJSn/ICUo/0lqgv94vO3/cbHg/z9gef8kKi3/Womq/3i97v9KdJP/LTtD/yEnKf8hJSj/ISYq/yo2Pv9flr3/AwUG/yEmKf9Ka4P/eLzt/3m+8P9xsuH/barV/22p1f9hlrz/OVNm/wQEBv8LDQ7/HyQn/0lqgv94u+3/cbHh/z9heP8hJyr/WYip/3i87v9LdJP/KjhB/x8lKP8gJSj/ICUo/yczO/9flb3/AwUG/yEmKv8jKC3/Ji40/0JmgP9glr//cbLh/22q1v9uqtb/TXiX/wIEBf8hJij/X5G1/3Cx3/9tqdX/barV/zxWav8CAwP/GR4h/yInKf8iJin/HyQn/x8iJ/8eJCb/ISYp/yIqLf8TFhj/AAEB/wIDA/8TFhj/Iygt/yElKf8MDg//AAAA/w0PEP8fIyT/EhUW/wABAf8AAAD/AgMD/wQFBf8CAgP/HyQn/wkLC/8CAgP/BAUF/wQEBf8XGhz/Iyku/yInK/8SFRf/AAEB/wAAAP8CAwP/BAUF/wICA/8gJSj/EhYY/wIEBP8JCwz/ISUp/yQpL/8jKC3/FRgb/wEBAf8EBQX/Fhga/xIUFf8AAQH/AgMD/xIUFf8gJSf/HyMm/wwODv8AAAD/DA4P/x0hI/8gJSj/HyMl/xIUFf8GBwj/AgID/wQFBf8CAgP/CgwN/x4jJv8RExb/AAEB/wIDA/8SFRf/Iycq/yMoLP8SFBb/AAEB/wIDA/8TFBb/IiYp/x4jJv8hJin/ISYo/yInKf8iJyn/EhYX/wEBAf8CAgP/ERMU/yAlJ/8fJCf/HyQn/xETFv8AAQH/AgMD/xIVGP8iJir/DA4P/wAAAP8OEBD/HSAj/yAjJ/8eJCb/ISYp/x0kJ/8ICQr/Iicr/yQpLf8UFxr/AQEB/wAAAP8CAwP/BAUF/wQFBf8JCgv/Fxob/yImKf8hJij/ICUo/xIVF/8BAQH/AgID/xESE/8fIyb/DA0O/wAAAP8NDxD/Gx4h/x8kJ/8fJCf/HyQn/xsfIv8HCAn/HiEm/x4kJv8hJin/ICcq/xEUFf8HCAn/AgMD/wUFBv8FBQb/DhAS/yEmKf8eIyb/CgsM/wIDA/8FBQX/BQUF/xYZGv8hJij/ICUo/yAlKP8fJCf/HyQn/yAkKP8gJCj/ISYp/yInKv8jKCr/Iygs/yInKv8kKS7/Iico/yInKv8iJyn/Iygq/yInKv8iJyr/Iicq/yInKf8iJyr/Iicp/yIoK/8jKCv/Iigr/yMpLP8oLjL/HiMm/x8kJ/8lKi7/Iicq/yInK/8iJyv/Iicq/yAlKP8fJCf/ICQo/yAkKP8hJin/Iicq/yMoKv8jKCz/Iicq/yQpLv8iJyj/Iicq/yInKf8jKCr/Iicq/yInKv8iJyr/Iicp/yInKf8iJyr/Iicq/yUqLv8jKC7/Iygt/yMoK/8iJyn/Iicq/yEmKf8gJSj/ICUo/yEmKf8hJin/HyQn/x8kJ/8gJCj/ICQo/yEmKf8iJyr/Iygq/yMoLP8iJyr/JCku/yInKP8iJyr/Iicp/yMoKv8iJyr/Iicq/yInKv8iJyn/Iicq/yInKf8iKCv/Iygr/yIoK/8jKSz/KC4y/x4jJv8fJCf/JSou/yInKv8iJyv/Iicr/yInKv8gJSj/HyQn/yAkKP8gJCj/ISYp/yInKv8jKCr/Iygs/yInKv8kKS7/Iico/yInKv8iJyn/Iygq/yInKv8iJyr/Iicq/yInKf8iJyn/Iicq/yInKv8lKi7/Iygu/yMoLf8jKCv/Iicp/yInKv8hJin/ICUo/yAlKP8hJin/ISYp/x8kJ/8fJCf/ICQo/yAkKP8hJin/Iicq/yMoKv8jKCz/Iicq/yQpLv8iJyj/Iicq/yInKf8jKCr/Iicq/yInKv8iJyr/Iicp/yInKv8iJyn/Iigr/yInKf8iJin/Iicp/yImKf8hJin/Iicq/yMoLP8jKCz/Iygs/yQpLf8jKCv/Iygr/yInK/8jKCv/JCkt/yQpLv8kKS7/Iygt/yMoLP8jKSz/Iigs/yQpLf8lKi7/JCkt/yQpLf8jJyz/Iygq/yQpLf8jJyr/ISYp/yInKv8iJyn/Iicp/yImKf8iJyn/IiYp/yEmKf8iJyr/Iygs/yMoLP8jKCz/JCkt/yMoK/8jKCv/Iicr/yMoK/8kKS3/JCku/yQpLv8jKC3/Iygs/yMpLf8jKCz/Iygr/yIoK/8iJyr/Iycr/yInKv8iJyn/IiYp/yIlKP8hJSj/Iicp/yInKf8iJyn/IiYp/yInKf8iJin/ISYp/yInKv8jKCz/Iygs/yMoLP8kKS3/Iygr/yMoK/8iJyv/Iygr/yQpLf8kKS7/JCku/yMoLf8jKCz/Iyks/yIoLP8kKS3/JSou/yQpLf8kKS3/Iycs/yMoKv8kKS3/Iycq/yEmKf8iJyr/Iicp/yInKf8iJin/Iicp/yImKf8hJin/Iicq/yMoLP8jKCz/Iygs/yQpLf8jKCv/Iygr/yInK/8jKCv/JCkt/yQpLv8kKS7/Iygt/yMoLP8jKS3/Iygs/yMoK/8iKCv/Iicq/yMnK/8iJyr/Iicp/yImKf8iJSj/ISUo/yInKf8iJyn/Iicp/yImKf8iJyn/IiYp/yEmKf8iJyr/Iygs/yMoLP8jKCz/JCkt/yMoK/8jKCv/Iicr/yMoK/8kKS3/JCku/yQpLv8jKC3/Iygs/yMpLP8iKCz/Iicp/yInKv8kKCz/Iygs/yQpLf8kKS3/JSku/yUqL/8lKi//Jiww/yYrL/8kKS3/JCkt/yQqLv8mLDD/Jiww/yYsMP8lKi//JCku/yUqLv8kKS7/JSov/yUqLv8kKS3/JCkt/yUpLv8jKCr/Iygr/yInKf8iJyr/Iicp/yInKf8iJyn/Iicq/yQoLP8jKCz/JCkt/yQpLf8lKS7/JSov/yUqL/8mLDD/Jisv/yQpLf8kKS3/JCou/yYsMP8mLDD/Jiww/yUqL/8kKS7/JSou/yQpLv8lKi//JCkt/yMoK/8iJyv/JSot/yInKv8jKCr/Iicp/yInKv8iJyn/Iicp/yInKf8iJyr/JCgs/yMoLP8kKS3/JCkt/yUpLv8lKi//JSov/yYsMP8mKy//JCkt/yQpLf8kKi7/Jiww/yYsMP8mLDD/JSov/yQpLv8lKi7/JCku/yUqL/8lKi7/JCkt/yQpLf8lKS7/Iygq/yMoK/8iJyn/Iicq/yInKf8iJyn/Iicp/yInKv8kKCz/Iygs/yQpLf8kKS3/JSku/yUqL/8lKi//Jiww/yYrL/8kKS3/JCkt/yQqLv8mLDD/Jiww/yYsMP8lKi//JCku/yUqLv8kKS7/JSov/yQpLf8jKCv/Iicr/yUqLf8iJyr/Iygq/yInKf8iJyr/Iicp/yInKf8iJyn/Iicq/yQoLP8jKCz/JCkt/yQpLf8lKS7/JSov/yUqL/8mLDD/Jisv/yQpLf8kKS3/JCou/yYsMP8mLDD/Jiww/yUqL/8kKS7/JSou/yQpLv8="
 
+const ARTEFACTS_URL = "https://sleepy-meh-alt-1.github.io/Archealogy-research-tracker/data/artefacts.json";
+
 let artefactData = null;
 
-async function loadArtefactData() {
-  try {
-    const res = await fetch("data/artefacts.json");
-    if (!res.ok) throw new Error(`HTTP ${res.status}`);
-
-    artefactData = await res.json();
-    console.log("Artefact data loaded:", artefactData);
-  } catch (err) {
-    console.error("Failed to load artefact data:", err);
-  }
+async function init() {
+  await loadArtefactData();   // wait until JSON is loaded
+  
 }
 
-window.addEventListener("DOMContentLoaded", async () => {
-  await loadArtefactData();
-  refreshStats(); // or whatever init logic you want
-});
+async function loadArtefactData() {
+  const res = await fetch(
+    "https://sleepy-meh-alt-1.github.io/Archealogy-research-tracker/data/artefacts.json",
+    { cache: "no-store" }
+  );
 
+  artefactData = await res.json();
+  console.log("Artefacts loaded:", Object.keys(artefactData.artefacts));
+}
 
-const sword = artefactData.artefacts["centurions_dress_sword"];
-console.log(sword.name);
+window.addEventListener("DOMContentLoaded", init);
+
 
 // -------------------------
 // Variables
@@ -154,11 +153,26 @@ window.addEventListener("DOMContentLoaded", () => {
 // Scan: find window -> build slots -> match items -> read qty
 // -------------------------
 function scan() {
+  // Safety: make sure artefact data is loaded
+  if (!artefactData || !artefactData.artefacts) {
+    console.warn("Artefact data not loaded yet");
+    return;
+  }
+
   rsBind = alt1.bindRegion(0, 0, alt1.rsWidth, alt1.rsHeight);
 
   const researchMatches = JSON.parse(
-    alt1.bindFindSubImg(rsBind, RESEARCH_ICON, 149, 0, 0, alt1.rsWidth, alt1.rsHeight)
+    alt1.bindFindSubImg(
+      rsBind,
+      RESEARCH_ICON,
+      149,
+      0,
+      0,
+      alt1.rsWidth,
+      alt1.rsHeight
+    )
   );
+
   const research = researchMatches[0];
   if (!research) {
     console.log("Research interface not found");
@@ -187,42 +201,58 @@ function scan() {
     }
   }
 
-  // Match items in each slot
   const results = [];
+
+  // Convert artefact map -> array once
+  const artefacts = Object.values(artefactData.artefacts);
 
   for (const slot of slots) {
     let found = null;
 
-    for (const item of items) {
+    for (const art of artefacts) {
+      if (!art.icon_damaged_base64) continue;
+
       const matchArr = JSON.parse(
-        alt1.bindFindSubImg(rsBind, item.icon, item.width, slot.x, slot.y, cellW, cellH)
+        alt1.bindFindSubImg(
+          rsBind,
+          art.icon_damaged_base64,
+          art.width ?? 28,   // fallback width if missing
+          slot.x,
+          slot.y,
+          cellW,
+          cellH
+        )
       );
+
       if (Array.isArray(matchArr) && matchArr.length > 0) {
-        found = item;
+        found = art;
         break;
       }
     }
 
     if (!found) continue;
 
+    // Highlight slot
     alt1.overLayRect(0xFF00FF00, slot.x, slot.y, cellW, cellH, 800, 1);
 
     const qty = readStackAt(slot.x, slot.y);
+
     results.push({
       slotIndex: slot.index,
       item: found.name,
-      category: found.category,
+      category: "artefact",
       qty,
-      tetraWorth: found.tetraWorth
+      tetraWorth: found.tetraWorth ?? 0
     });
   }
 
-    saveScan(results);
-    startResearchCooldown(Date.now()); // start 14h 38m from scan time
-    refreshStats();
+  saveScan(results);
+  startResearchCooldown(Date.now());
+  refreshStats();
 
   if (isHistoryOpen()) renderHistoryModal();
 }
+
 
 // -------------------------
 // History modal
